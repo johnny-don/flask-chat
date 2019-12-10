d0336d0744e3804bff6051e7061633a875d435ed
Ubuntu <ubuntu@ip-172-31-92-183.ec2.internal>
2019-12-10 19:32:41 +0000

    added chat textbox function and refractored code

--- run.py
+++ run.py
@@ -1,19 +1,43 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
+messages = []
 
-@app.route('/')
+
+def add_message(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username, "message": message})
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "To send a message use /USERNAME/MESSAGE"
-    
-@app.route('/<username>')
+    """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("index.html")
+
+
+@app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
-    return "Hi " + username
-    
-@app.route('/<username>/<message>')
-def send_message(username, message):
-    return "{0}: {1}".format(username, message)
-    
-    
-app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+    """Add and display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
+
+
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")), debug=False)
